11fada1ade3675cda9765bfa1f7f69b11cff275a
Pablo Tilotta <ptilotta@aper.com>
2023-03-11 20:38:54 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ptilotta/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ptilotta/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
