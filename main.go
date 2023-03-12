0c8519a8728750251d297203c7c6cac0d9571a5c
Pablo Tilotta <ptilotta@aper.com>
2023-03-11 22:59:10 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ptilotta/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
---
+++ variables/resto.go
@@ -0,0 +1,28 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+}
+
+func ConviertoaTexto(numero int) (bool, string) {
+	texto := strconv.Itoa(numero)
+	return true, texto
+}
