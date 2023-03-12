5eea318ee3c1576994f53cadee731bb85c0846a7
Pablo Tilotta <ptilotta@aper.com>
2023-03-12 12:18:47 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ptilotta/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto) */
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
